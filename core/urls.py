7cd36857cef62627c1ec5f93a3de33ffb522245d
forest <10756047@ntub.edu.tw>
2022-03-16 21:55:37 +0800

    make it better

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('index/', views.index),
+    path('', views.index),
     path('about/', views.about),
     path('coming/', views.coming),
     path('contact/', views.contact),
